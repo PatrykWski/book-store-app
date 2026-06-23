0094fe2caaaa747d14d87acc1a253308d0c57f35
PatrykWski <patrykwiszniewski96@gmail.com>
2026-06-23 17:37:55 +0200

    Changin ISBN parameter, deleting empty lines, changing the filed injection in bookStroeApplication class

--- book-store/src/main/java/bookstore/bookstore/BookStoreApplication.java
+++ book-store/src/main/java/bookstore/bookstore/BookStoreApplication.java
@@ -12,26 +12,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BookStoreApplication {
 
-    @Autowired
-    private BookService bookService;
-
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner commandLineRunner(BookService bookService) {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
-                        Book theWitcher = new Book();
-                        theWitcher.setAuthor("Andrzej Sapkowski");
-                        theWitcher.setPrice(BigDecimal.TEN);
-                        theWitcher.setTitle("The witcher");
-                        theWitcher.setIsbn("What is it");
-                        theWitcher.setDescription("A nice book about the monster killer");
-                        theWitcher.setCoverImage("");
-                        bookService.save(theWitcher);
+                Book theWitcher = new Book();
+                theWitcher.setAuthor("Andrzej Sapkowski");
+                theWitcher.setPrice(BigDecimal.TEN);
+                theWitcher.setTitle("The witcher");
+                theWitcher.setIsbn("9780316029186");
+                theWitcher.setDescription("A nice book about the monster killer");
+                theWitcher.setCoverImage("");
+                bookService.save(theWitcher);
                 System.out.println(bookService.findAll());
             }
         };
