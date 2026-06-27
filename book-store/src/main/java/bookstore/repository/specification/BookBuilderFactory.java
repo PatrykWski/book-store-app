9f69f8c5f69ae5f5ecce70b3bb5e3d453ab1ecbf
PatrykWski <patrykwiszniewski96@gmail.com>
2026-06-27 22:28:55 +0200

    creating search method in bookService and bookController

--- book-store/src/main/java/bookstore/repository/specification/BookBuilderFactory.java
+++ book-store/src/main/java/bookstore/repository/specification/BookBuilderFactory.java
@@ -1,4 +1,20 @@
 package bookstore.repository.specification;
 
+import bookstore.dto.CreateBookRequestDto;
+import bookstore.model.Book;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookBuilderFactory {
+    public Specification<Book> create (CreateBookRequestDto createBookRequestDto) {
+        return new SpecificationBuilder<Book>()
+                .with(BookSpecification.getByAuthor(createBookRequestDto.getAuthor()))
+                .with(BookSpecification.getByCoverImage(createBookRequestDto.getCoverImage()))
+                .with(BookSpecification.getByDescription(createBookRequestDto.getDescription()))
+                .with(BookSpecification.getByISBN(createBookRequestDto.getIsbn()))
+                .with(BookSpecification.getByPrice(createBookRequestDto.getPrice()))
+                .with(BookSpecification.getByTitle(createBookRequestDto.getTitle()))
+                .build();
+    }
 }
