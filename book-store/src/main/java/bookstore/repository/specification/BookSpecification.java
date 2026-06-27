89a2894b81bd2ab3b363e0298673b23afb0677ff
PatrykWski <patrykwiszniewski96@gmail.com>
2026-06-27 21:47:08 +0200

    Creating bookSpecification implementation with all methods from Book entity
    # Conflicts:
    #	book-store/src/main/java/bookstore/repository/BookRepository.java

---
+++ book-store/src/main/java/bookstore/repository/specification/BookSpecification.java
@@ -0,0 +1,49 @@
+package bookstore.repository.specification;
+
+import bookstore.model.Book;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.math.BigDecimal;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.jpa.domain.Specification;
+
+public class BookSpecification {
+
+    private static Specification<Book> create(String fieldName, Object value) {
+        if (value == null) {
+            return null;
+        }
+        return new Specification<Book>() {
+            @Override
+            public @Nullable Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(fieldName), value);
+            }
+        };
+    }
+
+    public static Specification<Book> getByTitle(String title) {
+        return create("title", title);
+    }
+
+    public static Specification<Book> getByAuthor(String author) {
+        return create("author", author);
+    }
+
+    public static Specification<Book> getByISBN(String isbn) {
+        return create("isbn", isbn);
+    }
+
+    public static Specification<Book> getByPrice(BigDecimal price) {
+        return create("price", price);
+    }
+
+    public static Specification<Book> getByDescription(String description) {
+        return create("description", description);
+    }
+
+    public static Specification<Book> getByCoverImage(String coverImage) {
+        return create("coverImage", coverImage);
+    }
+}
