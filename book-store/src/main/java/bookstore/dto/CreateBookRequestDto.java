992bfa10988893f18886d04eb99ad09193f0bc3c
PatrykWski <patrykwiszniewski96@gmail.com>
2026-06-29 18:47:29 +0200

    Deletin unused import

--- book-store/src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ book-store/src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -1,7 +1,6 @@
 package bookstore.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Getter;
