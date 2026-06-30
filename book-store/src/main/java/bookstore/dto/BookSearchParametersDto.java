9d82f825f03807a41f8a37a0f6b22ca8f53e2cef
PatrykWski <patrykwiszniewski96@gmail.com>
2026-06-30 19:04:38 +0200

    Create CustomGlobalExceptionHandler, edited NotValid method and added custom @ExceptionHandler for EntityNotFoundException. Edited BookSearchParametersDto by adding annotations for validator

--- book-store/src/main/java/bookstore/dto/BookSearchParametersDto.java
+++ book-store/src/main/java/bookstore/dto/BookSearchParametersDto.java
@@ -1,5 +1,8 @@
 package bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +10,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookSearchParametersDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @PositiveOrZero
     private BigDecimal minPrice;
+    @Positive
     private BigDecimal maxPrice;
+    @NotBlank
     private String isbn;
 }
