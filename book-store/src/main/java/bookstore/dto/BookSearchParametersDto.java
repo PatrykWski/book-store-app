1dd3de46afdd5c957d065855448990b125167a53
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-01 19:19:50 +0200

    Removing not null annotations from search DTO, removing Nullable annotation from custom global exception handler

--- book-store/src/main/java/bookstore/dto/BookSearchParametersDto.java
+++ book-store/src/main/java/bookstore/dto/BookSearchParametersDto.java
@@ -1,6 +1,5 @@
 package bookstore.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
@@ -10,14 +9,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookSearchParametersDto {
-    @NotBlank
     private String title;
-    @NotBlank
     private String author;
     @PositiveOrZero
     private BigDecimal minPrice;
     @Positive
     private BigDecimal maxPrice;
-    @NotBlank
     private String isbn;
 }
