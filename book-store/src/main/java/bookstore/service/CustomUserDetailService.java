e8335fb28558c856ba9d70e3ab6aaab7c00b780c
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-16 17:48:01 +0200

    Implement changes suggested by mentor

--- book-store/src/main/java/bookstore/service/CustomUserDetailService.java
+++ book-store/src/main/java/bookstore/service/CustomUserDetailService.java
@@ -1,22 +1,23 @@
 package bookstore.service;
 
-import bookstore.exception.EntityNotFoundException;
 import bookstore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class MyUserDetailService implements UserDetailsService {
+public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(@NonNull String email) throws EntityNotFoundException {
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
                 .orElseThrow(
-                        () -> new EntityNotFoundException("User not found"));
+                        () -> new UsernameNotFoundException(
+                                "User not found with email: " + email));
     }
 }
--- book-store/src/main/java/bookstore/service/UserServiceImpl.java
+++ book-store/src/main/java/bookstore/service/UserServiceImpl.java
@@ -2,10 +2,15 @@ package bookstore.service;
 
 import bookstore.dto.user.RegisterRequestDto;
 import bookstore.dto.user.UserResponseDto;
+import bookstore.exception.EntityNotFoundException;
 import bookstore.exception.RegistrationException;
 import bookstore.mapper.UserMapper;
+import bookstore.model.Role;
+import bookstore.model.RoleName;
 import bookstore.model.User;
+import bookstore.repository.RoleRepository;
 import bookstore.repository.UserRepository;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,14 +21,18 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(
             RegisterRequestDto registerRequestDto) throws RegistrationException {
         if (userRepository.findByEmail(registerRequestDto.getEmail()).isPresent()) {
-            throw new RegistrationException("Incorrect login or password");
+            throw new RegistrationException("A user with this email already exist");
         }
         User user = userMapper.toModel(registerRequestDto);
+        Role role = roleRepository.findByName(RoleName.USER).orElseThrow(
+                () -> new EntityNotFoundException("USER role not found"));
+        user.setRoles(Set.of(role));
         user.setPassword(passwordEncoder.encode(registerRequestDto.getPassword()));
         return userMapper.toDto(userRepository.save(user));
     }
