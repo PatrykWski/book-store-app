3a132c5a90ab0796dd5d7feb325d05ebc6e9f62b
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-18 22:30:37 +0200

    fix: moving null check in filter

--- book-store/src/main/java/bookstore/security/SecurityConfig.java
+++ book-store/src/main/java/bookstore/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers("/api/auth/**", "/swagger-ui/**",
-                                        "/v3/api-docs/**", "/swagger-ui.html")
+                                        "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/index.html")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
