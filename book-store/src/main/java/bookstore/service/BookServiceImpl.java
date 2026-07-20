42c50a21861482a7ef28ec1448071c26266fcf70
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-20 18:34:58 +0200

    Add transactional into book and category services, change few methods as suggested + deleted useless yaml changes and put it into one file

--- book-store/src/main/java/bookstore/service/BookServiceImpl.java
+++ book-store/src/main/java/bookstore/service/BookServiceImpl.java
@@ -6,13 +6,20 @@ import bookstore.dto.book.CreateBookRequestDto;
 import bookstore.exception.EntityNotFoundException;
 import bookstore.mapper.BookMapper;
 import bookstore.model.Book;
+import bookstore.model.Category;
 import bookstore.repository.BookRepository;
+import bookstore.repository.CategoryRepository;
 import bookstore.repository.specification.BookSpecificationBuilder;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,33 +31,51 @@ public class BookServiceImpl implements BookService {
 
     private final BookSpecificationBuilder bookSpecificationBuilder;
 
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
     @Override
     public BookDto save(CreateBookRequestDto createBookRequestDto) {
         Book book = bookMapper.toModel(createBookRequestDto);
+
+        Set<Category> categories = getCategories(createBookRequestDto);
+
+        book.setCategories(categories);
+
         Book savedBook = bookRepository.save(book);
         return bookMapper.toDto(savedBook);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Page<BookDto> findAll(Pageable pageable) {
         return bookRepository.findAll(pageable)
                 .map(bookMapper::toDto);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public BookDto findById(Long id) {
         Book book = getBookById(id);
         return bookMapper.toDto(book);
     }
 
+    @Transactional
     @Override
     public BookDto updateBookById(Long id, CreateBookRequestDto createBookRequestDto) {
         Book book = getBookById(id);
+
         Book updatedBook = bookMapper.updateBook(book, createBookRequestDto);
+
+        Set<Category> categories = getCategories(createBookRequestDto);
+
+        updatedBook.setCategories(categories);
+
         bookRepository.save(updatedBook);
         return bookMapper.toDto(updatedBook);
     }
 
+    @Transactional
     @Override
     public void deleteBookById(Long id) {
         Book book = getBookById(id);
@@ -62,6 +87,7 @@ public class BookServiceImpl implements BookService {
                 new EntityNotFoundException("Couldn't find book with id: " + id));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Page<BookDto> search(BookSearchParametersDto bookSearchParametersDto,
                                 Pageable pageable) {
@@ -69,5 +95,16 @@ public class BookServiceImpl implements BookService {
                 .create(bookSearchParametersDto);
         return bookRepository.findAll(booksSpecifications, pageable)
                 .map(bookMapper::toDto);
+    }
+
+    private Set<Category> getCategories(CreateBookRequestDto createBookRequestDto) {
+        Set<Category> categories = new HashSet<>(categoryRepository
+                .findAllById(createBookRequestDto.getCategoryIds()));
+
+        if (categories.size() != createBookRequestDto.getCategoryIds().size()) {
+            throw new EntityNotFoundException("One or more categories were not found.");
+        }
+
+        return categories;
     }
 }
