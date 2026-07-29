e66a96e70aa0856e0856116f16891675fe2fcece
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-29 13:09:34 +0200

    create-shopping-cart-service-impl-test

--- book-store/src/main/java/bookstore/dto/cartitem/AddBookRequestDto.java
+++ book-store/src/main/java/bookstore/dto/cartitem/AddBookRequestDto.java
@@ -2,6 +2,7 @@ package bookstore.dto.cartitem;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,18 @@ public class AddBookRequestDto {
     private Long bookId;
     @Positive
     private int quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AddBookRequestDto that = (AddBookRequestDto) o;
+        return Objects.equals(bookId, that.bookId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(bookId);
+    }
 }
